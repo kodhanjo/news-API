88832455c49469771a971caf09a9ea591210a624
kodhanjo <felix.osambo@student.moringaschool.com>
2021-08-10 11:51:45 +0300

    stored api keys

--- app/config.py
+++ app/config.py
@@ -1,8 +1,13 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
-    pass
+    NEWS_API_KEY= os.environ.get('NEWS_API_KEY')
+    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 
 
